ffff1521b8484ad684d3200a808e8807fa0d8628
xdylanm <you@example.com>
2024-10-02 20:47:58 -0700

    adding math parsing support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ author = 'Author'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["myst_parser"]
+myst_enable_extensions = ["dollarmath"]
 
 templates_path = ['_templates']
 exclude_patterns = []
